9fad11378ea03604ec16c5d81999ee63e8518ee0
Aayormitan <kolawoleabaz97.com>
2024-11-21 12:18:20 -0800

    Star wars API

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,45 @@
+#!/usr/bin/node
+
+
+const request = require('request');
+
+
+const movieId = process.argv[2];
+if (!movieId) {
+  console.error('Please provide a movie ID as an argument.');
+  process.exit(1);
+}
+
+const url = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
+
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  const data = JSON.parse(body);
+  const characters = data.characters;
+  
+  if (!characters) {
+    console.error('No characters found for this movie.');
+    return;
+  }
+  const characterPromises = characters.map(url => {
+    return new Promise((resolve, reject) => {
+      request(url, (err, res, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(body).name);
+	}
+      });
+    });
+  });
+
+  Promise.all(characterPromises)
+    .then(names => {
+      names.forEach(name => console.log(name))
+    })
+    .catch(err => console.error(err));
+});
